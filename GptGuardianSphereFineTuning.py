f02ba9cb483f2432ff7259c912797ee91be2c4c2
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-12-17 01:13:40 +0200

    update deploy

--- GptGuardianSphereFineTuning.py
+++ GptGuardianSphereFineTuning.py
@@ -478,4 +478,4 @@ def update_chat_feedback():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
