20de9f642b1a6fddc5033041346b4ad7cb5abf4a
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-12-17 01:38:15 +0200

    Fix app to listen on port 8000 and add Procfile

--- GptGuardianSphereFineTuning.py
+++ GptGuardianSphereFineTuning.py
@@ -477,5 +477,5 @@ def update_chat_feedback():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
