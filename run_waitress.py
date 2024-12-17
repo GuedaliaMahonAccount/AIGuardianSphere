10b89eb61e5db3f34250527e8a7be35d34b8d660
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2024-12-17 19:18:12 +0200

    update deploy

--- run_waitress.py
+++ run_waitress.py
@@ -1,13 +1,7 @@
 from waitress import serve
 from GptGuardianSphereFineTuning import app
-import sys
-import traceback
+import os
 
 if __name__ == "__main__":
-    try:
-        print(f"Python Path: {sys.path}")
-        print(f"Python Version: {sys.version}")
-        serve(app, host="0.0.0.0", port=8000)
-    except Exception as e:
-        print(f"Startup Error: {e}")
-        traceback.print_exc()
+    port = int(os.environ.get("PORT", 8000))
+    serve(app, host="0.0.0.0", port=port)
